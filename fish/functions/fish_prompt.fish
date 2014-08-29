d8c7cc3d12e8a656c296bc96b4a9c1332bd8a5fd
Sean Zhu <interestinglythere@gmail.com>
2014-08-29 16:02:02 -0700

    fish: update prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,7 +5,12 @@ function fish_prompt --description 'Write out the prompt'
 
   set -l last_status $status
 
-  if [ $TERM_PROGRAM = 'Apple_Terminal' ]; and [ -z '$INSIDE_EMACS' ]
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+
+  if [ "$TERM_PROGRAM" = 'Apple_Terminal' ]; and [ -z "$INSIDE_EMACS" ]
     printf "\e]7;file://%s\a" (echo -n $PWD | sed 's/ /%20/g')
   end
 
@@ -13,19 +18,30 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_prompt_normal (set_color normal)
   end
 
+  if not set -q __fish_prompt_cwd
+    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+  end
+
+  if [ -n "$SSH_CLIENT" ]; or [ -n "$SSH_TTY" ]
+    set_color $fish_color_autosuggestion[1]
+    echo -n "$USER@$__fish_prompt_hostname"
+    set_color normal
+    echo -n ' '
+  end
+
   # PWD
-  set_color $fish_color_cwd
+  set_color $fish_color_cwd[1]
   printf '%s' (prompt_pwd)
   set_color normal
 
   printf '%s ' (__terlar_git_prompt)
 
-  if not test $last_status -eq 0
-  set_color $fish_color_error
+  if not [ $last_status -eq 0 ]
+    set_color $fish_color_error[1]
   end
 
   echo
-  set_color $fish_color_autosuggestion
+  set_color $fish_color_autosuggestion[1]
   printf '%s ' (date +%H:%M)
   set_color normal
   printf '$ '
