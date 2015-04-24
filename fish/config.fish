a209b50020a721e4bdecc507697e2726d5180ff3
Sean Zhu <interestinglythere@gmail.com>
2015-04-24 02:27:57 -0700

    61b

--- fish/config.fish
+++ fish/config.fish
@@ -54,3 +54,9 @@ function funcsreset
     funcsnuke $argv
     and funcsupdate
 end
+
+# #
+
+set path_to_course_materials ~/Code/cs/grading/course-materials
+set -x CLASSPATH $CLASSPATH $path_to_course_materials/lib/* .
+set -x PATH $PATH $path_to_course_materials/lib/pygrader $path_to_course_materials/lib/reader_scripts
