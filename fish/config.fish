377972991ed432b9a453f64309ee1730fc63356c
Sean Zhu <interestinglythere@gmail.com>
2015-08-09 12:45:29 +0800

    fish abbr backcompat

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,13 @@
 not contains source (builtin -n)
 and alias source .
 
+# Backwards compatibility for fish <2.2.0
+not functions -q abbr
+and function abbr -a the_alias
+    set -e argv[1]
+    alias $the_alias "$argv"
+end
+
 for src in (find ~/.config/fish/config -name '*.fish')
     source $src
 end
