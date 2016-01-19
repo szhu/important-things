4bd6e179ccc80a330aea1d5e021a71c3b403ce32
Sean Zhu <interestinglythere@gmail.com>
2016-01-19 01:40:09 -0800

    stuff

--- fish/support/start-fish.bash
+++ fish/support/start-fish.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-PARENT="$(ps -o ucomm= -p $PPID | grep -oE '[^ ]+')"
-[[ "$PARENT" = 'login' || "$PARENT" = 'sshd' || "$PARENT" = 'su' || "$PARENT" = 'tmux' || ( -n "$DOCKER_HOST" && "$SHLVL" = 2 ) ]] &&
+# Check caller
+# (ps -o comm= -p $PPID | grep -qE '^(login|sshd|su|tmux|(.*/)?Atom Helper)$' || test -n "$DOCKER_HOST" -a "$SHLVL" = 2) &&
+# Check terminal
 [[ "$TERM" = "xterm-256color" || "$TERM" = "xterm-color" || "$TERM" = "screen" ]] &&
+# Check for fish
 for path in /usr/local/bin/fish /usr/bin/fish ~/.local/bin/fish; do
-    test -e "$path" && exec "$path" || true
+    test -e "$path" && exec "$path" 
 done
