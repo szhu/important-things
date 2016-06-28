b9d037566e294f43b7ca2e325be6ef19337f4264
Sean Zhu <interestinglythere@gmail.com>
2016-06-27 19:40:48 -0700

    Uninstall things I don't use

--- fish/config/cs61b.fish
+++
@@ -1,3 +0,0 @@
-set path_to_course_materials ~/Code/cs/grading/course-materials
-set -x CLASSPATH $CLASSPATH $path_to_course_materials/lib/* .
-set -x PATH $PATH $path_to_course_materials/lib/pygrader $path_to_course_materials/lib/reader_scripts
