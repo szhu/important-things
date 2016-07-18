c7ed6c97e7694a56d97165b458f208b70b16bb5b
Sean Zhu <interestinglythere@gmail.com>
2016-07-18 16:30:09 -0700

    Hyperterm and related issues

--- fish/support/start-fish.bash
+++ fish/support/start-fish.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check caller
-# (ps -o comm= -p $PPID | grep -qE '^(login|sshd|su|tmux|(.*/)?Atom Helper)$' || test -n "$DOCKER_HOST" -a "$SHLVL" = 2) &&
+(ps -o comm= -p $PPID | grep -qE '^(login|sshd|su|tmux|(.*/)?(Atom Helper|HyperTerm))$' || test -n "$DOCKER_HOST" -a "$SHLVL" = 2) &&
 # Check terminal
 [[ "$TERM" = "xterm-256color" || "$TERM" = "xterm-color" || "$TERM" = "screen" ]] &&
 # Check for fish
