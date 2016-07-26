f8ff5929789879c008e7490197ee05b8a2066ed1
Sean Zhu <interestinglythere@gmail.com>
2016-07-26 16:53:09 -0700

    launchservices

---
+++ launchservices/duti.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -eou pipefail
+IFS=$'\n\t'
+
+APP() {
+    _APP="${1}"
+}
+
+ROLE() {
+    _ROLE="${1}"
+}
+
+TYPE() {
+    local _TYPE="${1}"
+    echo duti -s "${_APP}" "${_TYPE}" "${_ROLE}"
+    duti -s "${_APP}" "${_TYPE}" "${_ROLE}"
+}
+
+
+APP  'com.sublimetext.3'
+ROLE 'all'
+
+    TYPE 'public.plain-text'
+    TYPE 'public.source-code'
+    TYPE 'public.script'
+    TYPE 'public.shell-script'
+    TYPE 'public.unix-executable'
+
+    TYPE 'public.python-script'
+    TYPE 'public.bash-script'
+    TYPE 'com.netscape.javascript-source'
+
+
+APP  'com.ridiculousfish.HexFiend'
+ROLE 'all'
+
+    TYPE 'public.data'
