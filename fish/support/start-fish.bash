443bc2cd3a7e2f2d96aee4d3c4094b454519a303
Sean Zhu <interestinglythere@gmail.com>
2016-09-12 10:33:47 -0700

    fish

--- fish/support/start-fish.bash
+++ fish/support/start-fish.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check caller
-(ps -o comm= -p $PPID | grep -qE '^(login|sshd|su|tmux|tmate|(.*/)?(Atom Helper|HyperTerm))$' || test -n "$DOCKER_HOST" -a "$SHLVL" = 2) &&
+(ps -o comm= -p $PPID | grep -qE '^(login|sshd|su|tmux|tmate|gnome-terminal|(.*/)?(Atom Helper|HyperTerm))$' || test -n "$DOCKER_HOST" -a "$SHLVL" = 2) &&
 # Check terminal
 echo "$TERM" | grep -qE '^(xterm|screen)(-(256)?color)?$' &&
 # Check for fish
