6ea4050c6dbbeda9a77380829067c22fb38b9a53
Sean Zhu <interestinglythere@gmail.com>
2016-09-13 19:01:09 -0700

    work work work work work

--- fish/support/start-fish.bash
+++ fish/support/start-fish.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Check caller
-(ps -o comm= -p $PPID | grep -qE '^(login|sshd|su|tmux|tmate|gnome-terminal|(.*/)?(Atom Helper|HyperTerm))$' || test -n "$DOCKER_HOST" -a "$SHLVL" = 2) &&
+(ps -o comm= -p $PPID | grep -qE '^(login|sshd|su|tmux|tmate|screen|gnome-terminal|(.*/)?(Atom Helper|HyperTerm))$' || test -n "$DOCKER_HOST" -a "$SHLVL" = 2) &&
 # Check terminal
-echo "$TERM" | grep -qE '^(xterm|screen)(-(256)?color)?$' &&
+echo "$TERM" | grep -qE '^(xterm|screen)(-(256)?color)?(-bce)?$' &&
 # Check for fish
 for path in ~/.local/bin/fish /usr/local/bin/fish /usr/bin/fish; do
     test -e "$path" && exec "$path" 
