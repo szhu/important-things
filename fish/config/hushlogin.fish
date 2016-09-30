11df7f7361a6608e5f5b1ab7322e13936a1d433f
Sean Zhu <interestinglythere@gmail.com>
2016-09-30 13:11:59 -0700

    spaces

--- fish/config/hushlogin.fish
+++ fish/config/hushlogin.fish
@@ -1,7 +1,7 @@
 if set -q NOHUSH
-  set -e NOHUSH
+    set -e NOHUSH
 else
-  test -e ~/.hushlogin
-  and sleep 0
-  and echo -ne  '\\033c'
+    test -e ~/.hushlogin
+    and sleep 0
+    and echo -ne  '\\033c'
 end
