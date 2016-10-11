b21850c676275656286b99d94bfb089fe5fc326f
Sean Zhu <interestinglythere@gmail.com>
2016-10-11 14:54:01 -0700

    Sometimes we don't want a git prompt.

--- fish/functions.src/fish/status.fish
+++ fish/functions.src/fish/status.fish
@@ -24,8 +24,31 @@ function status-time
   echo (date +%H:%M)
 end
 
+function status-git-forced
+  # Do nothing if not a git repo
+  set -l git_root (git rev-parse --show-toplevel ^/dev/null); or return
+  # Do nothing if $git_root/.git/noprompt exists
+  test -e $git_root/.git/noprompt; and return
+  # and __fish_git_prompt
+  # and false
+  python3 -c '
+from subprocess import call, check_output
+try:
+    call(["env", "NOHUSH=1", "fish", "-c", "__fish_git_prompt"], timeout=0.2)
+except:
+    print(" (%s)" % "git timeout")
+'
+end
+
 function status-git
-  which git >/dev/null; and __fish_git_prompt
+  # Do nothing if NO_PROMPT_GIT globally set
+  set -x -q NO_PROMPT_GIT; and exit
+
+  if test "$__status_git_updated" -le (date +%s)
+    set -g __status_git (status-git-forced)
+    set -g __status_git_updated (date +%s)
+  end
+  echo -n $__status_git
 end
 
 function status-git-nocolor
