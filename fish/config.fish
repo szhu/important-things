d077177a2d5fc4eabc8016552bb1904a69e44085
Sean Zhu <interestinglythere@gmail.com>
2017-07-28 14:31:56 -0400

    Add iterm integration

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,5 @@ find /dev/null >/dev/null
 for src in (find ~/.config/fish/config -name '*.fish')
     source $src
 end
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
