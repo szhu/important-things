c84202358107f466278bc70106e544e25baa8252
Sean Zhu <sean@affinity.co>
2018-03-05 15:15:32 -0800

    Move shims to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 status --is-interactive; or status --is-login; or exit
 
+set -x PATH ~/.local/bin ~/.local/opt/*/bin /usr/local/bin $PATH
+set -x LANG 'en_US.UTF-8'
+
 # Backwards compatibility for fish 2.0.0
 not contains source (builtin -n)
 and alias source .
@@ -9,6 +12,12 @@ not functions -q abbr
 and function abbr -a the_alias
     set -e argv[1]
     alias $the_alias "$argv"
+end
+
+# Abstracting this command so it can be changed to command -sq $argv once older versions
+# of fish are phased out
+function is_command
+  command -s $argv > /dev/null
 end
 
 # Fix for weird bug: find: .: Invalid argument
