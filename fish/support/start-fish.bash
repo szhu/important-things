5577b0278ccc7111d3966ebf08ac9ce34c8323a9
Sean Zhu <interestinglythere@gmail.com>
2018-03-13 12:23:36 -0700

    Robust detection for start-fish

--- fish/support/start-fish.bash
+++ fish/support/start-fish.bash
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# This line can really help debugging
-# osascript -e 'display notification "'"$(ps -o comm= -p $PPID)"'" with title "Invoked by"'
+# Only attempt to do this once per outer shell
+[[ "$FISH_AUTOINVOKED" != 1 ]] &&
 
-# Check caller
-(ps -o comm= -p $PPID | grep -qE '^((/usr/bin/)?login|sshd|su|script|tmux(: server)|tmate|screen|(gnome|xfce4)-terminal|(.*/)?(Atom Helper|Hyper(Term)?))$' ||
- test -n "$DOCKER_HOST" -a "$SHLVL" = 2 ||
- test "$C9_SH_EXECUTED" = 1 -a "$SHLVL" = 4) &&
 # Check terminal
+[[ -n "$PS1" ]] &&
 echo "$TERM" | grep -qE '^(xterm|screen)(-(256)?color)?(-bce)?$' &&
+
 # Check for fish
 for path in ~/.local/bin/fish /usr/local/bin/fish /usr/bin/fish; do
-    test -e "$path" && exec "$path"
+    [[ -e "$path" ]] && FISH_AUTOINVOKED=1 exec "$path"
 done
