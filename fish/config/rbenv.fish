31ca206b5564a766c92b7f0cd608e2aa7ce6be80
Sean Zhu <interestinglythere@gmail.com>
2019-05-13 10:15:46 -0700

    Fix rbenv for fish 3

--- fish/config/rbenv.fish
+++ fish/config/rbenv.fish
@@ -1,10 +1,16 @@
 is_command rbenv; or exit
-contains ~/.rbenv/shims $PATH; and exit
 
-# rbenv should have been loaded in .bash_profile or .bashrc, for GUI program compatibility.
-# Loading rbenv in this file is done only just in case.
-echo
-echo "Warning: rbenv is installed but was not loaded in a parent shell."
-echo rbenv init | env $SHELL
+if contains ~/.rbenv/shims $PATH
+  # Move it to the front. In fish 3.0, the order of PATH gets shuffled around when fish starts.
+  # We need ~/.rbenv/shims before /usr/bin and /usr/local/bin!
+  set -x PATH ~/.rbenv/shims $PATH
 
-status --is-interactive; and source (rbenv init -|psub)
+else
+  # rbenv should have been loaded in .bash_profile or .bashrc, for GUI program compatibility.
+  # Loading rbenv in this file is done only just in case.
+  echo
+  echo "Warning: rbenv is installed but was not loaded in a parent shell."
+  echo rbenv init | env $SHELL
+
+  status --is-interactive; and source (rbenv init -|psub)
+end
