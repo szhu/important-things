e7f65bbbe14b4d4c415e385945a4465c090490d8
Sean Zhu <interestinglythere@gmail.com>
2025-07-07 03:02:49 -0400

    Update start-fish.bash

--- fish/support/start-fish.bash
+++ fish/support/start-fish.bash
@@ -3,17 +3,17 @@
 # Only attempt to do this once per outer shell.
 [[ "$FISH_AUTOINVOKED" != 1 ]] &&
 
-# Check terminal interactivity. Programs that pipe into bash expect to interact with bash. But if
-# the interminal's input is interactive, then a human is on the other side, and fish can be used.
-# Use one of the following:
-# [[ -n "$PS1" ]] &&
-[[ -t 0 ]] &&
+  # Check terminal interactivity. Programs that pipe into bash expect to
+  # interact with the default shell. But if the terminal's input is interactive,
+  # then a human is on the other side, and fish can be used. Use one of the
+  # following:
+  [[ -t 0 ]] &&
 
-# Check terminal type.
-# Fish is buggy in certain, more basic shells. This isn't as common anymore.
-echo "$TERM" | grep -qE '^(xterm|screen)(-(256)?color)?(-bce)?$' &&
+  # Check terminal type.
+  # Fish is buggy in certain, more basic shells. This isn't as common anymore.
+  echo "$TERM" | grep -qE '^(xterm|screen)(-(256)?color)?(-bce)?$' &&
 
-# Check for fish, and start it.
-for path in ~/.local/bin/fish /usr/local/bin/fish /usr/bin/fish; do
-    [[ -e "$path" ]] && FISH_AUTOINVOKED=1 exec "$path"
-done
+  # Check for fish, and start it.
+  for fish_exe in ~/.local/bin/fish /opt/homebrew/bin/fish /usr/local/bin/fish /usr/bin/fish; do
+    [[ -e "$fish_exe" ]] && FISH_AUTOINVOKED=1 exec "$fish_exe"
+  done
